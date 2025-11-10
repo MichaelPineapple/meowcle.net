4571cae9d5a0e8b165a7bdf2ba41d94918c9b869
mcl <meowclewizlon@gmail.com>
2025-11-09 23:31:52 -0800

    Restructure

--- script/resume.js
+++
@@ -1,6 +0,0 @@
-function toggleTheme()
-{
-    let nextTheme = "light";
-    if (document.getElementById("toggle").checked == true) nextTheme = "dark";
-    document.getElementById("themestyle").href = "style/" + nextTheme + ".css";
-}
